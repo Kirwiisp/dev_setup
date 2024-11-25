81c5ad00a712333398acda8f2a6f273a7ec87946
Kirwiisp <59315476+Kirwiisp@users.noreply.github.com>
2024-11-25 19:39:49 +0100

    add aarch64Linux tar.gz

--- install.sh
+++ install.sh
@@ -10,33 +10,33 @@ install_packages() {
         apt)
             echo "Detected Debian/Ubuntu-based system. Using apt."
             sudo apt update 
-            sudo apt install -y gcc cmake tmux
+            sudo apt install -y tmux
             ;;
         pacman)
             echo "Detected Arch-based system. Using pacman."
             sudo pacman -Syu --noconfirm
-            sudo pacman -S --noconfirm gcc cmake tmux
+            sudo pacman -S --noconfirm tmux
             ;;
         apk)
             echo "Detected Alpine-based system. Using apk."
             sudo apk update && sudo apk upgrade
-            sudo apk add gcc cmake tmux
+            sudo apk add tmux
             ;;
         dnf)
             echo "Detected Fedora-based system. Using dnf."
             sudo dnf upgrade -y
-            sudo dnf install -y gcc cmake tmux
+            sudo dnf install -y tmux
             ;;
         yum)
             echo "Detected RHEL-based system. Using yum."
             sudo yum upgrade -y
-            sudo yum install -y gcc cmake tmux
+            sudo yum install -y tmux
             ;;
         zypper)
             echo "Detected openSUSE-based system. Using zypper."
             sudo zypper refresh
             sudo zypper update -y
-            sudo zypper install -y gcc cmake tmux
+            sudo zypper install -y tmux
             ;;
         *)
             echo "Unsupported package manager: $1"
@@ -62,17 +62,21 @@ else
     echo "No compatible package manager found. Exiting."
     exit 1
 fi
-# install nvim 
-git clone https://github.com/neovim/neovim
-cd neovim
-make CMAKE_BUILD_TYPE=Release
-make CMAKE_INSTALL_PREFIX=$HOME/local/nvim install
-make distclean
-make deps
 
-# add nvim to PATH
-echo 'export PATH=$HOME/local/nvim/bin:$PATH' >> ~/.bashrc
-export PATH=$HOME/local/nvim/bin:$PATH
+cd dev_setup
+
+# install nvim 
+arch=$(uname -m)
+if [ $arch = "aarch64" ] ; then	
+	tar -xzvf neovim_aarch64_Linux.tar.gz
+	sudo mv neovim/usr/local/bin/nvim /bin
+	sudo mv neovim/usr/local/share/nvim /usr/local/share
+else 
+	curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
+	sudo rm -rf /opt/nvim
+	sudo tar -C /opt -xzf nvim-linux64.tar.gz
+	export PATH="$PATH:/opt/nvim-linux64/bin"
+fi	
 
 # Confirm installation
 echo "Installation complete!"
@@ -80,7 +84,6 @@ command_exists nvim && echo "Neovim installed successfully."
 command_exists tmux && echo "Tmux installed successfully."
 
 cd ..
-rm -r neovim
 
 # Set defaults config ( number/relnumber/hlsearch)
 user_conf=$(whoami)
